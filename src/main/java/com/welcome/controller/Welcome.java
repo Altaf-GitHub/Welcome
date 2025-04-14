71cddad0a14b0f0e7ac3393d0f95e1b4f74d5020
Altaf-GitHub <ak327819@gmail.com>
2025-04-14 17:07:59 +0530

    made changes

---
+++ src/main/java/com/welcome/controller/Welcome.java
@@ -0,0 +1,19 @@
+package com.welcome.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Welcome {
+	
+	
+	@GetMapping("/welcome")
+	public String welcomeCOntroller()
+	{
+		
+		return "!!!!!!!!!!!!Welcome from Springboot!!!!!!!!!!!!!!!!";
+		
+	}
+}
